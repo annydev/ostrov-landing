90fa524d7e07cb4ce7324f5e34a1c505f09e9362
annydev <dandara.anna13@hotmail.com>
2019-11-18 18:29:54 +0200

    click mob

--- js/index.js
+++ js/index.js
@@ -103,8 +103,8 @@ function addCardsForTest() {
 addCardsForTest();
 
 $(document).ready(function(){
-    $(".offer-card").on('click', function () {
-        var cardClass = $(this).toggleClass("opened");
+    $(".offer-card").bind('touchstart', function (event) {
+        var cardClass = $(this.event).toggleClass("opened");
     });
 });
 
